951208aaff826f9c420952458df5abb12efe6527
João Pedro Matias <jpmmatias2018@outlook.com>
2021-05-01 20:13:02 -0300

    Created form for a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,15 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new; end
+
+  def create
+    render plain: params[:article]
   end
 end
