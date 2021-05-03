4727756a17151822d53585ee81450fef65f826b4
João Pedro Matias <jpmmatias2018@outlook.com>
2021-05-03 13:30:27 -0300

    Completed CRUD of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,39 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def new; end
+  def new
+    @article = Article.new
+  end
 
   def create
-    render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = 'Article was created successufuly'
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = 'Article was updated successufuly'
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    flash[:notice] =
+      @article.delete ? 'Article was deleted successufuly' : "Article wasn't deleted successufuly, an error occur"
+
+    redirect_to articles_path
   end
 end
